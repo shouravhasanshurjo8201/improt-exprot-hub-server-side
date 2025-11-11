9ae267a62dde84fa824ef045b9bdfa314ca9079e
shouravhasanshurjo8201 <shouravhasanshurjo8201@gmail.com>
2025-11-11 09:15:00 +0600

    MongoDB Connection

--- index.js
+++ index.js
@@ -2,16 +2,39 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.User_Name}:${process.env.MongoPassword}@cluster0.nivae1g.mongodb.net/?appName=Cluster0`;
 
 app.use(cors());
 app.use(express.json());
 
-
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+async function run() {
+    try {
+        await client.connect();
+        const ImportExportDB = client.db("ImportExportDB");
+        const myColl = ImportExportDB.collection("AllProducts");
+        
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
