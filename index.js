bc3c667bb3022459b38e22dbccb98dcec08ef4bd
shouravhasanshurjo8201 <shouravhasanshurjo8201@gmail.com>
2025-11-11 10:47:02 +0600

    Get Products ID

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
             res.send(allData)
         })
 
+        app.get('/Products/id/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await myColl.findOne(query);
+            res.send(result);
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
